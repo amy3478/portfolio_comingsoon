a6f587d8cea023ad06c3bc74a806ed2f07c45262
amy <yiming3478@gmail.com>
2017-01-19 10:51:53 -0500

    Added coming soon page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,89 @@
 import React, {Component} from "react";
-import NavBar from "./NavBar/NavBar.js";
 import "./App.css";
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      boundingClientRect: '',
+    }
+    
+    this.renderParticles = this.renderParticles.bind(this);
+  }
+
+  componentDidMount() {
+    let container = this.refs.particles;
+    const boundingClientRect = container.getBoundingClientRect();
+    this.setState({
+      boundingClientRect: boundingClientRect,
+    });
+
+    container.onmouseenter = function(e) {
+        container.className += ' active';
+    }
+    container.onmousemove = function(e) {
+        console.log("mouse move");
+        let bd = document.getElementsByTagName('body')[0],
+            bdsl = bd.scrollLeft,
+            pageX = e.pageX,
+            pageY = e.pageY,
+            w = container.clientWidth || container.offsetWidth || container.scrollWidth,
+            h = container.clientHeight || container.offsetHeight || container.scrollHeight,
+            offsetX = 0.52 - (pageX - boundingClientRect.left - bdsl)/w,
+            offsetY = 0.52 - (pageY - boundingClientRect.top / 2 + 160)/h,
+            dy = (pageY - boundingClientRect.top / 2 + 160) - h / 2,
+            dx = (pageX - boundingClientRect.left - bdsl) - w / 2,
+            yRotate = (offsetX - dx)*0.17,
+            xRotate = (dy - offsetY)*0.06,
+            imgCSS = 'rotateX(' + xRotate + 'deg) rotateY(' + yRotate + 'deg)';
+
+        if(container.className.indexOf(' active') !== -1){
+            imgCSS += ' scale3d(1.02,1.02,1.02)';
+        }
+        container.style.transform = imgCSS;
+        
+    };
+
+    container.onmouseleave = function(e) {
+        console.log("mouse leave");
+        container.className = container.className.replace(' active','');
+        container.style.transform = '';
+    };
+  }
+  
+  renderParticles() {
+    const containerW = this.state.boundingClientRect.width;
+
+    const count = containerW/5;
+
+    let particles = [];
+
+    for(let i = 0; i < count; i++) {
+      const size = Math.floor(Math.random() * (80 - 20) + 20) / 10;
+
+      const particleStyle= {
+        top: `${Math.random()*(80-20)+20}%`,
+        left: `${Math.random()*95}%`,
+        width: `${size}px`,
+        height: `${size}px`,
+        animationDelay: `${Math.random()*30}s`,
+      };
+
+      particles.push(<span className="particle" style={particleStyle}></span>);
+    }
+
+    return particles;
+  }
+  
   render() {
     return (
-      <div className="App">
-        <NavBar></NavBar>
-        App Works
+      <div className="container">
+        <span className="particletext particles" ref="particles">
+          Stay Tuned
+          {this.renderParticles()}
+        </span>
       </div>
     );
   }
