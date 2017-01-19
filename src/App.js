97dd6feff844c6e2f3fb99140e319e5e35b73d94
amy <yiming3478@gmail.com>
2017-01-19 11:19:37 -0500

    Added touch support

--- src/App.js
+++ src/App.js
@@ -20,15 +20,19 @@ class App extends Component {
       boundingClientRect: boundingClientRect,
     });
 
-    container.onmouseenter = function(e) {
+    const supportsTouch = 'ontouchstart' in window || navigator.msMaxTouchPoints;
+    console.log(supportsTouch);
+
+
+    if(supportsTouch){
+      container.ontouchstart = function(e) {
         container.className += ' active';
-    }
-    container.onmousemove = function(e) {
-        console.log("mouse move");
+      }
+      container.ontouchmove = function(e) {
         let bd = document.getElementsByTagName('body')[0],
             bdsl = bd.scrollLeft,
-            pageX = e.pageX,
-            pageY = e.pageY,
+            pageX = e.touches[0].pageX,
+            pageY = e.touches[0].pageY,
             w = container.clientWidth || container.offsetWidth || container.scrollWidth,
             h = container.clientHeight || container.offsetHeight || container.scrollHeight,
             offsetX = 0.52 - (pageX - boundingClientRect.left - bdsl)/w,
@@ -43,14 +47,45 @@ class App extends Component {
             imgCSS += ' scale3d(1.02,1.02,1.02)';
         }
         container.style.transform = imgCSS;
-        
-    };
+      }
+      container.ontouchend = function(e) {
+          container.className = container.className.replace(' active','');
+          container.style.transform = '';
+      };
+    } else {
+      container.onmouseenter = function(e) {
+          container.className += ' active';
+      }
+      container.onmousemove = function(e) {
+          console.log("mouse move");
+          let bd = document.getElementsByTagName('body')[0],
+              bdsl = bd.scrollLeft,
+              pageX = e.pageX,
+              pageY = e.pageY,
+              w = container.clientWidth || container.offsetWidth || container.scrollWidth,
+              h = container.clientHeight || container.offsetHeight || container.scrollHeight,
+              offsetX = 0.52 - (pageX - boundingClientRect.left - bdsl)/w,
+              offsetY = 0.52 - (pageY - boundingClientRect.top / 2 + 160)/h,
+              dy = (pageY - boundingClientRect.top / 2 + 160) - h / 2,
+              dx = (pageX - boundingClientRect.left - bdsl) - w / 2,
+              yRotate = (offsetX - dx)*0.17,
+              xRotate = (dy - offsetY)*0.06,
+              imgCSS = 'rotateX(' + xRotate + 'deg) rotateY(' + yRotate + 'deg)';
 
-    container.onmouseleave = function(e) {
-        console.log("mouse leave");
-        container.className = container.className.replace(' active','');
-        container.style.transform = '';
-    };
+          if(container.className.indexOf(' active') !== -1){
+              imgCSS += ' scale3d(1.02,1.02,1.02)';
+          }
+          container.style.transform = imgCSS;
+          
+      };
+
+      container.onmouseleave = function(e) {
+          console.log("mouse leave");
+          container.className = container.className.replace(' active','');
+          container.style.transform = '';
+      };
+    }
+    
   }
   
   renderParticles() {
@@ -71,7 +106,7 @@ class App extends Component {
         animationDelay: `${Math.random()*30}s`,
       };
 
-      particles.push(<span className="particle" style={particleStyle}></span>);
+      particles.push(<span className="particle" key={i} style={particleStyle}></span>);
     }
 
     return particles;
